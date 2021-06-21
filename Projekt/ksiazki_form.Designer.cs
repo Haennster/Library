46c252ab2690e7d894ae6dc2717703a571ca994c
Mikołaj Stal <24890@student.kpu.krosno.pl>
2021-06-21 18:12:21 +0200

    ksiazki

--- Projekt/ksiazki_form.Designer.cs
+++ Projekt/ksiazki_form.Designer.cs
@@ -29,19 +29,48 @@ namespace Projekt
         /// </summary>
         private void InitializeComponent()
         {
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.label1 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
             this.SuspendLayout();
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(12, 12);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(779, 272);
+            this.dataGridView1.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F);
+            this.label1.Location = new System.Drawing.Point(12, 306);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(70, 26);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "label1";
             // 
             // ksiazki_form
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(810, 350);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.dataGridView1);
             this.Name = "ksiazki_form";
             this.Text = "ksiazki_form";
+            this.Load += new System.EventHandler(this.ksiazki_form_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
+
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Label label1;
     }
 }
--- Projekt/ksiazki_form.cs
+++ Projekt/ksiazki_form.cs
@@ -16,5 +16,38 @@ namespace Projekt
         {
             InitializeComponent();
         }
+
+        private void ksiazki_form_Load(object sender, EventArgs e)
+        {
+            DaneEntities main = new DaneEntities();
+
+            var books = from book in main.Ksiazki
+                        join wyp in main.Wypozyczenia
+                        on book.id_ksiazka equals wyp.id_ksiazka
+                        join kat in main.Kategorie
+                        on book.id_kategoria equals kat.id_kategoria
+                        join author in main.Autorzy
+                        on book.id_autor equals author.id_autor
+                        select new
+                        {
+                            book.isbn,
+                            kategoria = kat.nazwa,
+                            book.tytul,
+                            book.opis,
+                            autor = author.imie + " " + author.nazwisko,
+                            wyp.data_wypozyczenia,
+                            wyp.data_oddania,
+                        };
+            dataGridView1.DataSource = books.ToList();
+
+            var number = from book in main.Ksiazki
+                         group book by book.id_ksiazka into countGroup
+                         select new
+                         {
+                             Count = countGroup.Count(),
+                         };
+            label1.Text = "Liczba książek w bibliotece: " + number.Count();
+        }
+
     }
 }
